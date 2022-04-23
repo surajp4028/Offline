be0aae585989901e57fe9144d3376fe758149d09
surajp4028 <76642613+surajp4028@users.noreply.github.com>
2022-04-23 14:33:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container'),
-    imgtag = container.querySelector('.status-box img'),
+    imgtaga = container.querySelector('.status-box .wifi'),
+    imgtagb = container.querySelector('.status-box .offline'),
     heading = container.querySelector('.status-box h3'),
     para = container.querySelector('.status-box p'),
     guide = container.querySelector('.guide');
@@ -13,7 +14,9 @@ window.onload = () => {
             if (xhr.status == 200 && xhr.status < 300) {
                 // console.log('online');
                 heading.innerText = "You're Online";
-                imgtag.src = "wifi.png";
+                // imgtag.src = "wifi.png";
+                imgtaga.classList.add('active');
+                imgtagb.classList.remove('active');
                 heading.classList.add('active');
 
                 para.innerText = "Hurray! Now you're connected to internet";
@@ -34,7 +37,9 @@ window.onload = () => {
     xhr.send();
     function offline(){
         heading.innerText = "You're Offline";
-        imgtag.src = "offline.png";
+        // imgtag.src = "offline.png";
+        imgtagb.classList.add('active');
+        imgtaga.classList.remove('active')
         heading.classList.remove('active');
 
         para.innerText = "Please connect to the internet";
@@ -43,6 +48,6 @@ window.onload = () => {
     }
    
 }
-setInterval(ajax, 10);
+setInterval(ajax, 5);
 
 }
